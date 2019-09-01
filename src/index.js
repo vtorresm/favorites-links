98c231a50f46edb423f191b6a057f9799ea06e34
Victor Torres Mori <vatorresm88@gmail>
2019-09-01 12:19:07 -0500

    Concluyendo configuración del servidor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 // Initializations
-const app = express()
+const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 4000)
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+
+app.set('view engine', '.hbs');
 
 // Middlewares
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.urlencoded({extends: false}));
+app.use(express.json());
 
 // Global Variables
-
+app.use((req, res, next) => {
+    next();
+});
 
 // Routes
-
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
 
 // Public
 
