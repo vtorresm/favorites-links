1bee36a097c5c0c424ce448152496d4e317d2afe
Victor Torres Mori <vatorresm88@gmail>
2019-09-02 22:24:04 -0500

    Creando el formulario y agregando estilos.

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,5 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', (req, res) => {
+    res.send('recibido')
+})
 
 module.exports = router;
