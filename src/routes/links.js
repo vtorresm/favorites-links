91d66c457ffda0865c378055a6edacf3877fd417
Victor Torres Mori <vatorresm88@gmail>
2019-09-02 22:40:58 -0500

    Guardando datos en la base de datos

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,17 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('recibido')
+router.post('/add', async (req, res) => {
+    //console.log(req.body);
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    //console.log(newLink);
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    res.send('recibido');
 })
 
 module.exports = router;
